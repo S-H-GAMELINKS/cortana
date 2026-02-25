4e8915e41ade29eefb19c810de1e35ad4116854d
S-H-GAMELINKS <gamelinks007@gmail.com>
2026-02-25 19:37:26 +0900

    Initial commit

---
+++ lib/cortana/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Cortana
+  VERSION = "0.1.0"
+end
---
+++ test/test_cortana.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestCortana < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Cortana::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
